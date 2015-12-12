312c4b352bc94f76f0113ecc335aa566f96f6887
Henry Stone <henryvstone@gmail.com>
2015-12-12 12:40:43 -0500

    Added controls for camera panning (up, down, left, right arrows). Made selection of the connectors possible (mouse - select and deselect, c - connect, a - deselect all, s - apply phyiscs). Added skybox and transparent shader for the build platform. Edits for the future include, placing individual parts under some master object when ther become connected, and reorienting objects so that their faces touch rather than just moving the points to connect to one another and making sure that connecting objects will not overlap. Also should add a way to toggle between connector selection and piece selection and move piece oriented controls to the piece selection mode. Also, add some overall manager that holds pointers to all pieces and connectors in order to streamline access. Finally toggleing physics on the bodies should disable the connector colliders, and enable the build plate collider. Possible consideration to make the build plate a full cube with normals facing inwards.

--- Assets/Camera Manipulation/CameraRotator.cs
+++ Assets/Camera Manipulation/CameraRotator.cs
@@ -5,6 +5,7 @@ public class CameraRotator : MonoBehaviour {
 	public float distance = 1;
 	public float period = 1;
 	private float angle = 0;
+	private float verticalAngle = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -13,8 +14,12 @@ public class CameraRotator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		angle += Mathf.PI * 2 / period * Time.deltaTime;
-		gameObject.transform.localPosition = new Vector3(-Mathf.Sin (angle)*distance, .5f, -Mathf.Cos (angle)*distance);
-		gameObject.transform.eulerAngles = new Vector3(35, angle*Mathf.Rad2Deg, 0);
+		angle += Mathf.PI * 2 / period * Time.deltaTime * (Input.GetKey(KeyCode.LeftArrow) ? 1 : 0);
+		angle -= Mathf.PI * 2 / period * Time.deltaTime * (Input.GetKey(KeyCode.RightArrow) ? 1 : 0);
+		verticalAngle += Mathf.PI * 2 / period * Time.deltaTime * (Input.GetKey(KeyCode.UpArrow) ? 1 : 0);
+		verticalAngle -= Mathf.PI * 2 / period * Time.deltaTime * (Input.GetKey(KeyCode.DownArrow) ? 1 : 0);
+
+		gameObject.transform.localPosition = new Vector3(-Mathf.Sin (angle)*distance*Mathf.Cos(verticalAngle), Mathf.Sin(verticalAngle)*distance, -Mathf.Cos (angle)*distance*Mathf.Cos(verticalAngle));
+		gameObject.transform.eulerAngles = new Vector3(verticalAngle*Mathf.Rad2Deg, angle*Mathf.Rad2Deg, 0);
 	}
 }
---
+++ Assets/Robot Pieces/ConnectorGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConnectorGenerator : MonoBehaviour {
+	public Vector3[] positionsToBeGenerated;
+	public GameObject ConnectorModel;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < positionsToBeGenerated.Length; i++) {
+			GameObject c = (GameObject) Instantiate (ConnectorModel);
+			c.transform.parent = this.transform;
+			c.transform.localPosition = positionsToBeGenerated[i];
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
